233107060c84ce28c303a851540d2280807ff98a
G. Van Slyke <garrisonslyke11@gmail.com>
2026-05-16 17:29:42 -0600

    Finished the prep assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
+        Random ran = new Random();
+        for (int i = 0; i < 20; i ++)
+        {
+            string output = "";
+            fraction.SetNumerator(ran.Next(1, 101));
+            fraction.SetDenominator(ran.Next(1, 101));
+            output += $"Fraction {i + 1}: string: {fraction.GetFractionString()} Number: {fraction.GetDecimal()}";
+            Console.WriteLine(output + "\r\n");
+        }
     }
 }
